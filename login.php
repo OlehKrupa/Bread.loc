211cca0363c32b4cd5a887c223c9aaac76728ad9
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2023-03-02 23:04:31 +0200

    Curl not working

--- login.php
+++ login.php
@@ -21,11 +21,34 @@ if (!empty($_POST)){
 	}
 
 	if (empty($error)){
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL, "http://test.loc/Api.loc/");
+		curl_setopt($ch, CURLOPT_POST, 1);
+		$data = array(
+			'user' => $_POST['user'],
+			'password' => $_POST['password']
+		);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$response = curl_exec($ch);
+		curl_close($ch);
+
+		$result = json_decode($response, true);
+
+		if ($result['success']) {
+			echo "Response message: " . $result['message'];
+		} else {
+			echo "Error message: " . $result['message'];
+		}
+
+		/*
 		$_SESSION['user']=htmlspecialchars($_POST['user']);
 		$_SESSION['logined']=1;
 		
 		header("location: /index.php");
 		die();
+		*/
 	}
 }
 
